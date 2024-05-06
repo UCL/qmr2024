7724c32a35ac863ba13b08d75f04c4a4ec9e24be
David Atkinson <D.Atkinson@ucl.ac.uk>
2024-05-06 11:35:45 +0800

    Updated comments in code. Added copy of calcADC as comments.

--- qmrdiff2024.m
+++ qmrdiff2024.m
@@ -1,6 +1,9 @@
 function qmrdiff2024
 % QMRDIFF Quantitative MRI Diffusion Challenge 2024
+%
+% David Atkinson  D.Atkinson@ucl.ac.uk
 
+% Edit for your location of data
 dinput = '/Users/davidatkinson/Library/CloudStorage/OneDrive-UniversityCollegeLondon/data/DiffusionChallenge/00005-MR-ep2d_basic_clinical_ORIG' ;
 
 dlist = dir(fullfile(dinput,'*')) ;
@@ -19,7 +22,8 @@ dinfo = dfparse(ffns) ;
 bvs = unique([dinfo.DiffusionBValue]) ;
 bvs(bvs==0) = [] ;
 
-[vb0,mb0]    = d2mat(dinfo,{'slice','bv'},'bv',0,'op','fp') ;
+% d2mat returns volume and information about data
+[vb0, mb0]    = d2mat(dinfo,{'slice','bv'},'bv',0,'op','fp') ;
 [vbnz, mbnz] = d2mat(dinfo,{'slice','bv','bdirec','ddty'},'bv',bvs,'bdirec',[1 2 3],'ddty',1,'op','fp') ;
 
 % Combine the directions using geomteric mean of signals
@@ -30,6 +34,7 @@ vbiso = geomean(vbnz,5) ; % [ny nx nslice nbv]
 vbiso = cat(4,vb0,vbiso) ;
 bvs = [0 bvs] ;
 
+% calcADC is in another repo but copied below for info.
 [ADC, S0] = calcADC(vbiso, bvs) ;
 sviewer(ADC*1e6,mbnz)
 sviewer(vbiso,mbnz)
@@ -117,4 +122,101 @@ end
 cb = colorbar(hamap, 'southoutside', 'AxisLocation','in', 'FontSize',12) ;
 cmap = colorcet('L16') ;
 colormap(cmap) ;
+
+% Copy of calcADC used here.
+% function [ADC, S0] = calcADC(volb, bvVec, bvUse)
+% % calcADC Calculate ADC by linear fitting to log of data (multiple b
+% % values)
+% %
+% % [ADC, S0] = calcADC(volb, bvVec)
+% % [ADC, S0] = calcADC(volb, bvVec, bvUse)
+% %
+% % volb [ny nx nz nbv]  or  [ny nx nbv]
+% % bvVec should have length nbv
+% % bvUse defaults to bvVec, otherwise only the bvalues present in bvUse will
+% % be included in the ADC calculation
+% %
+% % Solves the following for S0 and ADC using a least squares fit 
+% % to the log of the data:
+% %   S=S0 exp(-ADC.b)
+% % i.e. 
+% % ln(S) = ln(S0) - ADC.b
+% % ln(S) = ADC.(-b) + ln(S0)
+% % Y = mX + c
+% % Y = [X 1] [m c]'
+% % X = A\B ;
+% % ADC = X(1,:) ;
+% % S0 = X(2,:) ;
+% % 
+% % Examples
+% % dinfo = datparse ;
+% % [volb, matp] = d2mat(dinfo,{'slice','bv'},'op','fp') ;
+% % [ADC, S0] = calcADC(volb, matp.bvVec) ;
+% %  displayADC(ADC*1e6, 2000)
+% %
+% % dinfo = dfparse(ffn) ;
+% % [vb0, mb0] = d2mat(dinfo,{'slice','bv'},'bv',0,'op','fp%) ;
+% % bvs = unique([dinfo.DiffusionBValue]) ;
+% % [vbiso, mbiso] = d2mat(dinfo,{'slice','bv','ddty'},'ddty',2,'bv',bvs(2:end),'op','fp%) ;
+% % volb = cat(4, vb0, vbiso);
+% % [ADC, S0] = calcADC(volb, bvs) ;
+% %
+% %
+% %
+% % David Atkinson
+% %
+% % See also dwi2ADC
+% 
+% % Reshape so that single slice is still 4D (inc bvaues)
+% ndv = ndims(volb) ;
+% if ndv==3
+%     volb = reshape(volb,[size(volb,1) size(volb,2) 1 ndv]) ;
+% end
+% 
+% volb(~isfinite(volb)) = 0 ;
+% loc = volb < 0 ;
+% if ~isempty(loc)
+%     warning('Negative signal in input to calcADC')
+%     volb(loc) = 0 ;
+% end
+% 
+% 
+% % Restrict bvalues if requested.
+% if nargin == 3
+%    [bvIntersect, ia] = intersect(bvVec,bvUse) ;
+%    if length(bvUse)~=length(bvIntersect) 
+%        warning(['Not all b-values in bvUse are in data'])
+%    end
+% 
+%    bvVec = bvVec(ia) ;
+%    volb = volb(:,:,:,ia) ;
+% end
+% 
+% [ny nx nz nbv] = size(volb) ;
+% if nbv ~= length(bvVec) 
+%     error(['Last dimension of volb must be same as number of bvalues'])
+% end
+% 
+% A = cat(2, -bvVec(:), ones([length(bvVec) 1])) ;
+% 
+% B = log(volb) ;
+% B = reshape(B,[ny*nx*nz nbv]) ;
+% 
+% Btest = sum(B,2) ;
+% [row] = find(~isfinite(Btest)) ;
+% B(row, :) = 0 ;
+% 
+% B = B.' ;
+% 
+% X = A\B ;
+% 
+% ADC = X(1,:) ;
+% S0 = X(2,:) ;
+% 
+% ADC(row) = 0 ;
+% S0(row) = 0 ;
+% 
+% ADC = reshape(ADC,[ny nx nz]) ;
+% S0 = exp(reshape(S0,[ny nx nz])) ;
+
 
